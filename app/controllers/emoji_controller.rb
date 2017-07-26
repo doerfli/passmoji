28fb8449469926d998ef396102c7a23c95e5a3c4
Marc Doerflinger <marc@doerf.li>
2017-07-26 22:19:45 +0200

    styling

--- app/controllers/emoji_controller.rb
+++ app/controllers/emoji_controller.rb
@@ -1,12 +1,15 @@
 class EmojiController < ApplicationController
 
   def index
-    allowed_characters = Emoji.all.select{ |e| ! e.raw.nil? }
-    @password = ''
-    len = (params['length'] || '8').to_i
-    (0...len).each { |i|
-        @password += allowed_characters.sample(1).first.raw
-    }
+    @password = nil
+    unless params['length'].nil?
+      allowed_characters = Emoji.all.select{ |e| ! e.raw.nil? }
+      @password = ''
+      len = (params['length'] || '8').to_i
+      (0...len).each { |i|
+          @password += allowed_characters.sample(1).first.raw
+      }
+    end
     logger.info @password
   end
 
