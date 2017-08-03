c8e438a4ce6f8674af6f7ca70350c2b62bbb665d
Marc Doerflinger <marc@doerf.li>
2017-08-03 22:41:04 +0200

    add csp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :load_moji
+  before_action :csp_header
 
   def load_moji
     @moji_index = Gemojione::Index.new
+  end
+
+  def csp_header
+    @csp_nonce = SecureRandom.hex
+    csp = "default-src 'self';"
+    csp << "script-src 'self' 'nonce-#{@csp_nonce}' www.google-analytics.com;"
+    csp << "img-src 'self' www.google-analytics.com;"
+    response.headers['Content-Security-Policy'] = csp
   end
 end
